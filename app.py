52ea30e5cffe61823a3af405db4df3c95522f620
hiroshi <kohama1619@yahoo.co.jp>
2021-09-21 23:53:42 +0900

    提取配置信息

--- app.py
+++ app.py
@@ -6,32 +6,9 @@ from redis import StrictRedis
 from flask_session import Session
 from flask_wtf.csrf import CSRFProtect
 
+from config import Config
+
 app = Flask(__name__)
-
-"""
-相关配置信息
-1. 数据库配置
-2. redis配置: 缓存访问频率高的内容，存储session信息，图片验证码，短信验证码
-3. session配置: 用来保存用户的登录信息
-4. csrf配置: 保护app，放置csrf攻击
-"""
-
-class Config():
-    DEBUG = True
-    SECRET_KEY = "alkdsjfepiruijp"
-    # 数据库配置
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:aisin-aw23@localhost:3306/info36"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # redis配置信息
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-
-    # Session配置信息
-    SESSION_TYPE = 'redis' #设置session存储类型
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT) # 指定session存储的redis服务器
-    SESSION_USE_SIGNER = True #设置签名存储
-    PERMANENT_SESSION_LIFETIME = timedelta(days=2)
 
 app.config.from_object(Config)
 
@@ -62,4 +39,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000)
---
+++ config.py
@@ -0,0 +1,49 @@
+from datetime import timedelta
+from redis import StrictRedis
+
+"""
+相关配置信息
+1. 数据库配置
+2. redis配置: 缓存访问频率高的内容，存储session信息，图片验证码，短信验证码
+3. session配置: 用来保存用户的登录信息
+4. csrf配置: 保护app，放置csrf攻击
+"""
+
+# 设置配置文件（基类配置信息）
+class Config():
+    DEBUG = True
+    SECRET_KEY = "alkdsjfepiruijp"
+    # 数据库配置
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:aisin-aw23@localhost:3306/info36"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # redis配置信息
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    # Session配置信息
+    SESSION_TYPE = 'redis' #设置session存储类型
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT) # 指定session存储的redis服务器
+    SESSION_USE_SIGNER = True #设置签名存储
+    PERMANENT_SESSION_LIFETIME = timedelta(days=2)
+
+# 可以生成Config的子类，并且复写其中的属性
+
+# 开发环境配置信息
+class DevelopConfig(Config):
+    pass
+
+# 生产(线上)环境配置信息
+class ProductConfig(Config):
+    pass
+
+# 测试环境配置信息
+class TestConfig(Config):
+    pass
+
+# 提供一个统一的访问入口
+config_dict = {
+    'develop':DevelopConfig,
+    'product':ProductConfig,
+    'test':TestConfig
+}
