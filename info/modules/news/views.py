2ce8eda329623d7dc2e3f6e7cbdda52fd35bf447
hiroshi <kohama1619@yahoo.co.jp>
2021-11-07 12:05:41 +0900

    新闻详情页展示

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,5 +1,19 @@
-from . import news_blue
+from flask import jsonify, current_app, render_template
 
-@news_blue.route('news_detail')
-def news_detail():
-    return 'news'
+from . import news_blue
+from ...models import News
+
+
+@news_blue.route('/<int:news_id>')
+def news_detail(news_id):
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno='1', errmsg='获取新闻失败')
+
+    data = {
+        'news_info': news.to_dict() if news else '',
+    }
+
+    return render_template('news/detail.html', data=data)
